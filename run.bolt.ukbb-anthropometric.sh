4b233da628ea9886dde0b2677d3691c567608cdf
Sara L Pulit <sara.pulit@gmail.com>
2018-04-04 18:00:05 +0100

    Create run.bolt.ukbb-anthropometric.sh

---
+++ run.bolt.ukbb-anthropometric.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#$ -cwd
+#$ -pe shmem 10
+
+## Script for running GWAS using BOLT-LMM
+
+pheno=$1 ## bmi, whr, whradjbmi
+ldsc=$2 ## ukbb.10k.imp.r2, baseline
+grm=$3 ## imputed
+subset=$4 ## pruned
+sex=$5 ## combined, females, males
+
+echo "************************************************************"
+echo "Started at: "`date +"%m/%d/%Y %H:%M:%S"`
+echo "************************************************************"
+
+mygrm=/path/to/grm/$grm/$subset
+
+## Set up the phenotype information
+if [ $pheno == "bmi" ]; then
+    myPhenoCol="res_bmi_inv"
+elif [ $pheno == "whr" ]; then
+    myPhenoCol="res_whr_inv"
+elif [ $pheno == "whradjbmi" ]; then
+    myPhenoCol="res_whrAdjBMI_inv"
+fi
+
+## Set up the ldscore file
+if [ $ldsc == "ukbb.10k.imp.r2" ]; then
+    ldscore_file="/path/to/ldscores/sensitivityAnalysis/10k/ukbb.imputed.pruned.v2.10k.a.l2.ldscore.gz"
+    ldscore_header="L2"
+elif [ $ldsc == "baseline" ]; then
+    ldscore_file="/path/to/ldscores/sensitivityAnalysis/1000G_Phase3_baselineLD_ldscores/baselineLD.all_chr.l2.ldscore.gz"
+    ldscore_header="baseL2"
+    ldscore_header="L2"
+fi
+
+echo $myPhenoCol $ldscore_file
+
+/apps/well/bolt-lmm/2.3/bolt \
+    --lmmInfOnly \
+    --bim $mygrm/ukbb.$grm.snps.v2.hrc.grm.$subset.chr{1:22}.bim \
+    --bed $mygrm/ukbb.$grm.snps.v2.hrc.grm.$subset.chr{1:22}.bed \
+    --fam $mygrm/ukbb.$grm.snps.v2.hrc.grm.$subset.chr1.fam \
+    --exclude=/path/to/snps/ukbb.non-hrc.snps.txt \
+    --remove /path/to/samples/bolt.in_plink_but_not_imputed.FID_IID.968.txt \
+    --remove /path/to/samples/ukbb.dropSamples.sex-mismatch.txt \
+    --remove /path/to/samples/ukbb.dropSamples.sampleMissingness-0.05.txt \
+    --remove /path/to/samples/ukbb.dropSamples.het.missing.outliers.txt \
+    --remove /path/to/samples/ukbb.dropSamples.consentRevoked.txt \
+    --phenoFile=/path/to/phenotypes/UKBiobank.BMI_WHR_WHRadjBMI.inv_residuals.$sex.v2.bolt.txt \
+    --phenoCol=$myPhenoCol \
+    --covarFile=/path/to/phenotypes/UKBiobank.BMI_WHR_WHRadjBMI.covariates.v2.bolt.txt \
+    --covarCol=ArrayType \
+    --LDscoresFile=$ldscore_file \
+    --LDscoresCol=$ldscore_header \
+    --geneticMapFile=/path/to/recombination/maps/genetic_map_hg19_withX.txt.gz \
+    --numThreads=10 \
+    --statsFile=/path/to/$pheno/ukbb.fat-distn.$pheno.bolt.association.$sex.$grm.$ldsc.stats.grm.infinitesimal.txt \
+    --bgenFile=/path/to/ukbiobank/data/ukb_imp_chr{1:22}_v2.bgen \
+    --bgenMinMAF=1e-4 \
+    --bgenMinINFO=0.3 \
+    --sampleFile=/path/to/sample_names.txt \
+    --statsFileBgenSnps=/path/to/$pheno/ukbb.fat-distn.$pheno.bolt.association.$sex.$grm.$ldsc.stats.bgen.infinitesimal.txt \
+    --verboseStats
+
+echo "Ended at: "`date +"%m/%d/%Y %H:%M:%S"`
